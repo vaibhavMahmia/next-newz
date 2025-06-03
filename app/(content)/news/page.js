3311f284c1426e4435ef59fcb0bc5f902d5ace28
vaibhavM <vaibhav@kali.kali>
2025-06-03 20:47:52 +0530

    __server_side_data_fetching__: app/(content)/news/page.js

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,39 +1,16 @@
-'use client';
+import { headers } from "next/headers";
 import { NewsList } from "@/components/news-list";
-import { useEffect, useState } from "react";
 
-const NewsPage = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState();
-    const [news, setNews] = useState();
+const NewsPage = async () => {
+    const response = await fetch(`http://localhost:3000/api/news`);
+    if (!response.ok) throw new Error("Failed to fetch news!");
+    const news = await response.json();
 
-    useEffect(() => {
-        const fetchNews = async () => {
-            setIsLoading(true);
-            const response = await fetch('/api/news');
-
-            if(!response.ok){
-                setError('Failed to fetch news !');
-                setIsLoading(false);
-            }
-
-            const news = await response.json();
-            setIsLoading(false);
-            setNews(news);
-        }
-
-        fetchNews();
-    }, []);
-
-    if(isLoading) return <p>Loading...</p>;
-    if(error) return <p>{error}</p>;
-
-    let newsContent;
-    if(news) newsContent = <NewsList news={news} />;
-
-    return <>
-        <h1>News Page</h1>
-        {newsContent}
-    </>;
-}
+    return (
+        <>
+            <h1>News Page</h1>
+            <NewsList news={news} />
+        </>
+    );
+};
 export default NewsPage;
